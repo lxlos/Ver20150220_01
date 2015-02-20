d05042721b65631582d83052c1312ba97a8fdff7
Alex Los <nekopanda21@gmail.com>
2015-02-20 09:51:08 +0100

    Verifica di informatica

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
+const int N=5;
+
 int main(int argc, char** argv) {
-
-    return (EXIT_SUCCESS);
+    int vettore[N];
+    int Maggiorenni;
+    int i=1;
+   
+    
+    while(i<=5) {
+    printf("Inserisci l' eta' della %da persona: ", i);
+    scanf("%d", &vettore[i-1]);   
+    i=i+1;
+    }
+    i=0;
+    Maggiorenni=0;
+    while(i<=5) {
+        if(vettore[i-1]>=18) {
+            Maggiorenni=Maggiorenni+1;
+        }
+        i=i+1;
+    }
+    printf("I maggiorenni sono: %d", Maggiorenni);
+    
+    return (Maggiorenni-1);
 }
-
